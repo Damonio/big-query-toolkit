3527ba35d109278ff00587083c62d974366e4d25
Damian Perez Guirado <damian.perez.guirado@gmail.com>
2024-10-21 22:36:17 -0400

    Removing cross origin as it local is configurable and only local will be allowed

--- big-query-migration-tool-web-app/src/main/java/com/damonio/migration/web/migration/GenericMigrationController.java
+++ big-query-migration-tool-web-app/src/main/java/com/damonio/migration/web/migration/GenericMigrationController.java
@@ -1,14 +1,12 @@
 package com.damonio.migration.web.migration;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/migrate")
